b9e0f65fb6139172658bb3df11f2a3d25158e395
bhaagiKenpachi <bhagath.reddy@dojima.network>
2025-03-21 18:39:00 +0530

    Add updates to crowd-funding example

--- examples/crowd-funding/src/lib.rs
+++ examples/crowd-funding/src/lib.rs
@@ -57,9 +57,6 @@ pub enum Operation {
         chain_name: String,
         address: String,
     },
-    AddChains {
-      funds_data:
-    },
     RemoveChain {
         chain_name: String,
     },
